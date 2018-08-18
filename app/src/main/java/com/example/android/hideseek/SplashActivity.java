c76eb46b1b5eed2f5a85223913c2b2cf46e4d199
Shubham-11 <Shubham-11@users.noreply.github.com>
2018-08-18 12:27:16 +0530

    small change in splash screen

--- app/src/main/java/com/example/android/hideseek/SplashActivity.java
+++ app/src/main/java/com/example/android/hideseek/SplashActivity.java
@@ -6,7 +6,7 @@ import android.os.Handler;
 import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
-    private static int SPLASH_TIME= 4000;
+    private static int SPLASH_TIME= 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
