2795326361a8820583360753c2e8bbe83fddf40a
Shubham-11 <Shubham-11@users.noreply.github.com>
2018-08-18 11:31:22 +0530

    added splash screen

---
+++ app/src/main/java/com/example/android/hideseek/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.hideseek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME= 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Code to start timer and take action after the timer ends
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mySuperIntent);
+                /* This 'finish()' is for exiting the app when back button pressed
+                 *  from Home page which is ActivityHome
+                 */
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+
+
+}
