da15272486aa57db92c77dce928c7eb736b363b1
Shubham-11 <Shubham-11@users.noreply.github.com>
2018-08-19 11:15:59 +0530

    added get data to splash activity

--- app/src/main/java/com/example/android/hideseek/SplashActivity.java
+++ app/src/main/java/com/example/android/hideseek/SplashActivity.java
@@ -6,14 +6,11 @@ import android.os.Handler;
 import android.content.Intent;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
